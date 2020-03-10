68e8fec77e59f7ebdeacbc7b7526af1e42a932f4
ZEZE <55626553+EZ-Super@users.noreply.github.com>
2020-03-10 21:32:50 +0800

    Add files via upload

---
+++ 1~100亂數搜尋/Module1.vb
@@ -0,0 +1,30 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim a(100), y, x, i As Integer
+        Dim find As Boolean
+        Randomize()
+
+        For i = 1 To 100
+            a(i) = Int(Rnd() * 100) + 1
+            Console.Write(i & "=" & a(i) & ",")
+        Next
+        Console.Write("請輸入搜尋資料")
+        x = Console.ReadLine()
+        find = False
+        For i = 1 To 100
+            y += 1
+            If x = a(i) Then
+                Console.WriteLine("第" & y & "個為" & a(i))
+                Console.WriteLine("共搜尋了" & i & "次")
+                find = True
+                Exit For
+            End If
+        Next
+        If Not find Then
+            Console.WriteLine("沒有搜尋到" & x)
+        End If
+        Console.Read()
+    End Sub
+
+End Module
